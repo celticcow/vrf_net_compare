04b5785eb19891605c5f10e5a4fb540f942c9fa6
Celticcow <gregory.dunlap@gmail.com>
2020-10-02 08:43:24 -0500

    basic file read

--- vcompare.py
+++ vcompare.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 import csv
+import sys
+import argparse
+import ipaddress
 
 
 """
@@ -8,4 +11,30 @@ read in exiting group / list of networks
 compare with output of vrf collections
 """
 
+def build_ip_list(filename):
+    print("in build_ip_list")
+    debug = 1
 
+    with open(filename) as csvreader:
+        reader = csv.reader(csvreader, delimiter=',', quotechar='|')
+
+        for row in reader:
+            net = row[0]
+            print(net)
+
+#endof build_ip_list()
+
+def main():
+    print("begin network list compare")
+
+    file1 = "/home/gdunlap/Code/python/vrf_net_compare/ind1.csv"
+    file2 = "/home/gdunlap/Code/python/vrf_net_compare/ind2.csv"
+
+    build_ip_list(file1)
+    build_ip_list(file2)
+
+    print("end")
+
+if __name__ == "__main__":
+    main()
+#end of program
